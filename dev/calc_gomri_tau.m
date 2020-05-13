4f80cab32c46db50f7419cc3fdae26ad4e6ce91c
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2020-05-12 21:18:31 -0300

    added filters to script, need to do butterworth

--- dev/calc_gomri_tau.m
+++ dev/calc_gomri_tau.m
@@ -3,6 +3,7 @@ addpath(genpath('../'))
 floats = {'f7939', 'f7940', 'f7941', 'f7942', 'f7943', 'f7944', 'f7945', 'f8081', 'f8082', 'f8083'};
 
 fprintf('-------------------------- RESULTS --------------------------\n\n')
+fprintf('------------------------- NO FILTER -------------------------\n\n')
 fprintf('flID\tN\tN_pt\tm_p\tsd_p\tN_rhot\tm_rho\tsd_rho\n')
 fprintf('--------------------------------------------------------------\n')
 
@@ -13,6 +14,61 @@ for ii=1:numel(floats)
     PDEN = real(sw_pden(S, T, P, 0) - 1000);
 
     pres_tau = calculate_tau(t, P, DOXY, 'tlim', [40,100], 'tres', 0.5);
+
+    fprintf('%s\t', floatID)
+    fprintf('%d\t', size(T, 1))
+    fprintf('%d\t', numel(pres_tau))
+    fprintf('%3.1f\t%3.3f\t', median(pres_tau), std(pres_tau))
+
+    pden_tau = calculate_tau(t, PDEN, DOXY, 'tlim', [40,100], 'tres', 0.5, 'zlim', [22,26.5], 'zres', 0.1);
+
+    fprintf('%d\t', numel(pden_tau))
+    fprintf('%3.1f\t%3.3f\n', median(pden_tau), std(pden_tau))
+    
+end
+
+fprintf('------------------------ RUNNING MEAN ------------------------\n\n')
+fprintf('flID\tN\tN_pt\tm_p\tsd_p\tN_rhot\tm_rho\tsd_rho\n')
+fprintf('--------------------------------------------------------------\n')
+
+for ii=1:numel(floats)
+
+    floatID = floats{ii};
+    [S, T, P, DOXY, t] = load_float_data(floatID);
+    PDEN = real(sw_pden(S, T, P, 0) - 1000);
+
+    f_DOXY = movmean(DOXY, 7);
+
+    pres_tau = calculate_tau(t, P, DOXY, 'tlim', [40,100], 'tres', 0.5);
+
+    fprintf('%s\t', floatID)
+    fprintf('%d\t', size(T, 1))
+    fprintf('%d\t', numel(pres_tau))
+    fprintf('%3.1f\t%3.3f\t', median(pres_tau), std(pres_tau))
+
+    pden_tau = calculate_tau(t, PDEN, DOXY, 'tlim', [40,100], 'tres', 0.5, 'zlim', [22,26.5], 'zres', 0.1);
+
+    fprintf('%d\t', numel(pden_tau))
+    fprintf('%3.1f\t%3.3f\n', median(pden_tau), std(pden_tau))
+    
+end
+
+fprintf('------------------------- BUTTERWORTH ------------------------\n\n')
+fprintf('flID\tN\tN_pt\tm_p\tsd_p\tN_rhot\tm_rho\tsd_rho\n')
+fprintf('--------------------------------------------------------------\n')
+
+% [b,a] = butter(n,Wn);
+
+for ii=1:numel(floats)
+
+    floatID = floats{ii};
+    [S, T, P, DOXY, t] = load_float_data(floatID);
+
+    % f_DOXY = filter(b,a,DOXY);
+
+    PDEN = real(sw_pden(S, T, P, 0) - 1000);
+
+    pres_tau = calculate_tau(t, P, f_DOXY, 'tlim', [40,100], 'tres', 0.5);
 
     fprintf('%s\t', floatID)
     fprintf('%d\t', size(T, 1))
